df79e73fabe4e157ea93236102ad542c2ee275a7
lina-ejb <linakryzneva@gmail.com>
2024-05-22 20:50:13 +0300

    HW 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,7 +1,7 @@
 // code
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 
-switch (rule) {
+switch (true) {
     case rule.length < 25:
         console.log('Все таки нет правил без исключения')
         break
@@ -9,10 +9,7 @@ switch (rule) {
         console.log('У меня 100 пудов все получится')
         break
 
-    case rule.length === 25:
-        console.log('50 на 50')
-        break
     default:
-        console.log('Все сламалася!!')
+        console.log('50 на 50')
         break
 }
