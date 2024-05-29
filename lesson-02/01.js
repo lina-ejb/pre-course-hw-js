818b6e54960a08dab11556e64c651ea0c0dab28d
lina-ejb <linakryzneva@gmail.com>
2024-05-29 15:14:41 +0300

    HW 7 (week 2)

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,13 +1,7 @@
 // code
-let passportMarried = {
-    name: "Petr",
-    surname: "Petrov",
-    address: {
-        country: "USA",
-        city: "Bobryisk"
-    }
-};
-const passportMarried2 = {...passportMarried, married: true, address: {...passportMarried.address}};
 
-console.log(passportMarried);
-console.log(passportMarried2);
+function doArrow(value) {
+
+    console.log(value);
+}
+doArrow('Передай 5')
